bb58b51eeae6f939ca24b9d6225e59c0e4c79976
Hassan Ilyas <hassan_ilyas0@yahoo.com>
2021-08-28 03:22:06 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://mongo-user:MongoDB123PL@cluster0.d8obg.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'very secret key',
+    PORT: 5000
+} 
